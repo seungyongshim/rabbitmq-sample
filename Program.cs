7433a84b81d6bd853966e6d41199f6c8e0ea1e4b
심승용 <seungyong.shim@mirero.co.kr>
2019-07-10 10:25:49 +0900

    Sample

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Text;
+
+namespace Receive
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+
+            using (var connection = factory.CreateConnection("ThisIsMyName"))
+            {
+                Console.WriteLine(" Press [enter] to exit.");
+                Console.ReadLine();
+            }
+        }
+    }
+}
